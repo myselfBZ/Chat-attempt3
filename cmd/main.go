cd16ec913ed3c8c742d9b2f29136c246ca9fb875
Boburmirzo <symonbobur@gmail.com>
2024-07-28 10:22:08 +0500

    Some more little changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	ws "github.com/myselfBZ/Chat2/WebSocket"
+)
 
 
 func main()  {
-    fmt.Println("Hello world")
+    h := ws.NewHandler()
+    mux := http.NewServeMux()
+    mux.HandleFunc("/rooms/{roomId}", h.JoinRoom)
+    mux.HandleFunc("POST /rooms", h.CreateRoom)
+    log.Println("Listening...")
+    log.Fatal(http.ListenAndServe(":8080", mux))
 }
